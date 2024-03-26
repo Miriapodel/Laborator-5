d0a8b000094874171c93b6bc2db93f97427ee0e0
Bobei Vlad <64199808+Miriapodel@users.noreply.github.com>
2024-03-26 14:39:19 +0200

    exercitii

--- src/stringuri/Application.java
+++ src/stringuri/Application.java
@@ -12,5 +12,20 @@ public class Application {
         System.out.println("abcab".matches("[abc]{5}"));
         System.out.println("abcabcabcabc".matches("[abc]{5,}"));
         System.out.println("abcabc".matches("[abc]{5,10}"));
+
+        String str = "Odata creat un sir de caractere cu continutul sau nu mai poate fi modificat.";
+        System.out.println("Lungimea string-ului este: " + str.length());
+        System.out.println("Are doar litere mari si mici: " + str.matches("[a-zA-Z]+"));
+
+
+        var cuvinte = str.trim().split(" ");
+        System.out.println(cuvinte[cuvinte.length / 2 - 1] + " " + cuvinte[cuvinte.length / 2 ]);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(str);
+        stringBuilder.reverse();
+
+        System.out.println(stringBuilder);
     }
 }
